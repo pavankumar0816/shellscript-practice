8af5e8849af821351ac9ef3eb9a71d9e45ce45da
pavankumar0816 <manikantapavankumarpuppala@gmail.com>
2026-02-04 10:42:08 +0530

    actions

--- demo1.sh
+++ demo1.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-PERSON1="Ram"
-PERSON2="Raj"
+PERSON1=$1
+PERSON2=$2
+
+echo "Script file name: $0"
+echo "Number of args passed: $#"
+echo "PWD: $PWD"
+echo "Current User: $USER"
+echo "Home directory of current user: $HOME"
+echo "PID of the script: $$"
+
 
 echo "$PERSON1:: Hello $PERSON2, How are you?"
 echo "$PERSON2:: Hi $PERSON1, I am good. How about you?"
