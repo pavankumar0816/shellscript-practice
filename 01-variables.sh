1821f126ec1932354dad041949968ba1712d9b91
pavankumar0816 <manikantapavankumarpuppala@gmail.com>
2026-02-04 10:48:52 +0530

    actions

--- 01-variables.sh
+++ 01-variables.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-username=$1
-password=$2
 
 echo "Enter ur name:"
-read $username
-
+read username
 echo "Your name is $username"
+
 echo "Enter ur password:"
-read -s $password
-echo "Your password is $password"
+read -s pwd
+echo "Your password is $pwd"
