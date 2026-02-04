527eaab01f2132b391089ef576285785b906716f
pavankumar0816 <manikantapavankumarpuppala@gmail.com>
2026-02-04 10:53:10 +0530

    actions

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-timestamp=$(date)
+timestamp=$(date "+%y-%m-%d %H:%M:%S")
 
 echo "Script executed at $timestamp"
 starttime=$(date +%s)
 sleep 5
 endtime=$(date +%s)
+echo "Script Ended at $timestamp"
 duration=$(($endtime-$starttime))
 echo "Time taken : $duration"
 
