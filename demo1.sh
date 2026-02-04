a63f5b4e192e35d224c6a24ad612ff0c1e56ac7b
pavankumar0816 <manikantapavankumarpuppala@gmail.com>
2026-02-04 10:39:00 +0530

    actions

---
+++ demo1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+PERSON1="Ram"
+PERSON2="Raj"
+
+echo "$PERSON1:: Hello $PERSON2, How are you?"
+echo "$PERSON2:: Hi $PERSON1, I am good. How about you?"
+echo "$PERSON1:: I am doing great. What are you learning $PERSON2?"
+echo "$PERSON2:: I am learning Shell Scripting. what about you?"
+
