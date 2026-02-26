73993a3a5a725365bcf7e7897a99afce0381bdf1
pavankumar0816 <manikantapavankumarpuppala@gmail.com>
2026-02-26 10:31:27 +0530

    practice

--- 02-variables.sh
+++ 02-variables.sh
@@ -9,7 +9,7 @@ echo "Script executed at $timestamp"
 starttime=$(date +%s)
 echo "Start Time: $starttime"
 sleep 5
-endtime=$(date +%s)
+endtime=$(date +%s) # %s ==> It returns the number of seconds, unix timestamp
 echo "Script Ended at $endtime"
 duration=$(($endtime-$starttime))
 echo "Time taken : $duration"
