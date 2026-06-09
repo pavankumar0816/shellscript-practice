a82a7cf09bad4eba91123cac3bd08afb1dffdc76
pavankumar0816 <manikantapavankumarpuppala@gmail.com>
2026-06-09 21:59:53 +0530

    shell

--- demo2.sh
+++ demo2.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-TIMESTAMP=$(date)
-echo "$TIMESTAMP"
+echo "All args passed to script: $@"
+echo "Number of args passed: $#"
+echo "Script name: $0"
+echo "First arg: $1"
+echo "Third arg: $3"
+echo "All args as a single string: $*"
+echo "Present working directory: $PWD"
+echo "Current user: $USER"
+echo "Home directory of current user: $HOME"
+echo "PID: $$"
+sleep 5 &
+echo "PId of recently executed background process: $!"
+echo "Exist status of last executed command: $?"
 
-start=$(date +%S)
-echo "Script executed at: $start"
-sleep 5
-end=$(date +%S)
-echo "Script ended at: $end"
 
-duration=$(($end - $start))
-
-echo "Total duration: $duration"
 
