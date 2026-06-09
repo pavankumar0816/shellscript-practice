f877e3964154701edbf484744fad0116252fbb91
pavankumar0816 <manikantapavankumarpuppala@gmail.com>
2026-06-09 22:08:15 +0530

    shell

--- demo2.sh
+++ demo2.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 
 userid=$(id -u)
-echo "$userid"
+
+if [ $userid -ne 0 ]; then
+   echo "You must run it as root user"
+   exit 1 
+fi
+
+echo "Installing nginx"
+dnf install nginx -y
+
+if [ $? -ne 0 ]; then
+   echo "Failed to install Nginx"
+   exit 1
+else 
+    echo "Installed nginx successfully"
+fi
+
+
