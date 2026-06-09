87a18184fbfc106ed203a016b6015d571a169277
pavankumar0816 <manikantapavankumarpuppala@gmail.com>
2026-06-09 21:51:49 +0530

    shell

--- demo2.sh
+++ demo2.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-TIMESTAMP=$(date +"%y-%m-%d %H:%M:%S")
+TIMESTAMP=$(date)
 echo "$TIMESTAMP"
+
+start=$(date +%s)
+echo "Script executed at: $start"
+sleep 5
+end=$(date +%s)
+echo "Script ended at: $end"
+
+duration=$(($end - $start))
+
+echo "Total duration: $duration"
 
