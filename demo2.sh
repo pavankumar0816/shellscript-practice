3f033f4d9296c60eb0dbc8aafd246ded8742d0be
pavankumar0816 <manikantapavankumarpuppala@gmail.com>
2026-06-09 22:03:58 +0530

    shell

--- demo2.sh
+++ demo2.sh
@@ -1,18 +1,4 @@
 #!/bin/bash
 
-echo "All args passed to script: $@"
-echo "Number of args passed: $#"
-echo "Script name: $0"
-echo "First arg: $1"
-echo "Third arg: $3"
-echo "All args as a single string: $*"
-echo "Present working directory: $PWD"
-echo "Current user: $USER"
-echo "Home directory of current user: $HOME"
-echo "PID: $$"
-sleep 5 &
-echo "PId of recently executed background process: $!"
-echo "Exist status of last executed command: $?"
-
-
-
+userid=$(id -u)
+echo "$userid"
