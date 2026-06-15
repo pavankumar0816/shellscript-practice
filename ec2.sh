9d866999faf99ac4720c5dde539f655cabf182d4
pavankumar0816 <manikantapavankumarpuppala@gmail.com>
2026-06-15 14:44:40 +0530

    ec2

---
+++ ec2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+AMI_ID="ami-0220d79f3f480ecf5"
+SG_ID="sg-01cbaa06a27135f05"
+
+
+for instance in $@
+do
+   instance_id=$(aws ec2 run-instances \
+    --image-id "$AMI_ID" \
+    --instance-type "t3.micro" \
+    --security-group-ids "$SG_ID" \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+    --query 'Instances[0].InstanceId' \
+    --output text
+    )
+     echo "Instance Id" $instance_id created for $instance"
+done    
+
