0f075c1e1be5ba72a654a606047a32485b7870ef
pavankumar0816 <manikantapavankumarpuppala@gmail.com>
2026-06-15 15:35:59 +0530

    ec2

--- count_instances.sh
+++ count_instances.sh
@@ -2,7 +2,7 @@
 AMI_ID="ami-0220d79f3f480ecf5"
 
 instances=$(aws ec2 describe-instances \
-  --filters "Name=instance-state-name,Values=Running" \
+  --filters "Name=instance-state-name,Values=running" \
   --query 'Reservations[*].Instances[*].InstanceId' \
   --output text)
 
