50390daa84dc048c5db9e02f802008d75d776238
pavankumar0816 <manikantapavankumarpuppala@gmail.com>
2026-06-15 15:23:54 +0530

    ec2

--- ec2.sh
+++ ec2.sh
@@ -14,6 +14,7 @@ do
     --instance-type "t3.micro" \
     --security-group-ids $SG_ID \
     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+    --query 'Instances[0].InstanceId' \
     --output text 
     )
 
