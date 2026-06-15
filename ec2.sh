9adf9dc20bd5e0d936290961a0a3b1cae6d9831f
pavankumar0816 <manikantapavankumarpuppala@gmail.com>
2026-06-15 15:06:26 +0530

    ec2

--- ec2.sh
+++ ec2.sh
@@ -2,6 +2,8 @@
 
 AMI_ID="ami-0220d79f3f480ecf5"
 SG_ID="sg-01cbaa06a27135f05"
+hosted_zone_id="Z00391083QXL8RYXR03CG"
+DOMAIN_NAME="pmpkdev.online"
 
 
 for instance in $@
@@ -23,13 +25,42 @@ do
         --query 'Reservations[*].Instances[*].PublicIpAddress' \
         --output text
         )
+        RECORD_NAME="$DOMAIN_NAME"
     else
       IP=$(aws ec2 describe-instances \
         --instance-ids $instance_id \
         --query 'Reservations[*].Instances[*].PrivateIpAddress' \
         --output text
       )
+      RECORD_NAME="$instance.$DOMAIN_NAME"
     fi
     echo "Instance Id: $instance_id is created for $instance"
     echo "IP Address: $IP"
+
+
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $hosted_zone_id \
+    --change-batch  '
+        {
+  "Comment": "Updating record",
+  "Changes": [
+    {
+        "Action": "UPSERT",
+        "ResourceRecordSet": {
+            "Name": "$RECORD_NAME",
+            "Type": "A",
+            "TTL": 1,
+            "ResourceRecords": [
+            {
+                "Value": "$IP"
+            }
+            ]
+        }
+        }
+    ]
+    }
+
+    '
+
+
 done    
