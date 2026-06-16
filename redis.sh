d322b8c8e2cdb9d00b70be0cdda4e803384c739c
pavankumar0816 <manikantapavankumarpuppala@gmail.com>
2026-06-16 21:57:39 +0530

    roboshop

--- redis.sh
+++ redis.sh
@@ -39,7 +39,7 @@ else
 fi
 
 sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
-VALIDATE $? "Allowing Remote Connections"
+validate $? "Allowing Remote Connections"
 
 systemctl enable redis &>>$log_file
 systemctl start redis 
