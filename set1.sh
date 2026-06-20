e458674c1f6f427478c4f266e91c6adea511b4da
pavankumar0816 <manikantapavankumarpuppala@gmail.com>
2026-06-20 15:34:19 +0530

    shell

--- set1.sh
+++ set1.sh
@@ -6,13 +6,15 @@ userid=$(id -u)
 logs_folder="/var/log/practice"
 log_file="$logs_folder/$0.log"
 
+mkdir -p $logs_folder
+
 if [ $userid -ne 0 ]; then
-    echo "Run with root user"
+    echo "Run with root user" | tee -a $log_file
     exit 1
 fi
 
 for package in $@
 do
-    dnf list installed $package
-    
+    dnf list installed $package &>>$log_file
+
 done
