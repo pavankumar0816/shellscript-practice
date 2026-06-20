1c85f8b35e9c0cf5a899abe8f5f87846143a374c
pavankumar0816 <manikantapavankumarpuppala@gmail.com>
2026-06-20 16:04:06 +0530

    shell

--- set2.sh
+++ set2.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 
-set -u
+# NAME="Pavan"
+
+# demo(){
+#     echo "Learning Shell"
+# }
+# demo
+#set      # ==> Displaying variables and functions 
+
+
+
+set -u # handling undefined variables
 
 NAME=$1
 Course="Shell"
 echo $NAME
-echo $course
+# echo $course
 echo $Course
+
+#Managing Positional Parameters using set
+set -- apple mango orange
+
+echo "First Fruit: $1"
+echo "Second Fruit: $2"
+echo "Third Fruit: $3"
