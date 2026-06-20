43e4b866d86f6d9dda7a16dc15eb8abe93487e35
pavankumar0816 <manikantapavankumarpuppala@gmail.com>
2026-06-20 16:51:21 +0530

    shell

--- delete-old-logs.sh
+++ delete-old-logs.sh
@@ -7,6 +7,7 @@ N="\e[0m"
 
 LOGS_DIR=/home/ec2-user/app-logs
 LOGS_FILE="$LOGS_DIR/$0.log"
+START_TIME=$(date +%s)
 
 if [ ! -d $LOGS_DIR ]; then
    echo -e "$LOGS_DIR $R Directory Not Exist $N" 
@@ -22,3 +23,7 @@ do
    rm -f $filepath
    echo "Deleted Files: $filepath" | tee -a $LOGS_FILE
 done <<< $FILES_TO_DELETE
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(($END_TIME - $START_TIME))
+echo -e "$G Total time taken for deleting the logs: $Y $TOTAL_TIME $N"
