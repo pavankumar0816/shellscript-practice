84ff99908f0b5786fd80f4ea3f1aa4931482a668
pavankumar0816 <manikantapavankumarpuppala@gmail.com>
2026-06-20 16:59:54 +0530

    shell

--- delete-old-logs.sh
+++ delete-old-logs.sh
@@ -9,7 +9,7 @@ LOGS_DIR=/home/ec2-user/app-logs
 LOGS_FILE="$LOGS_DIR/$0.log"
 START_TIME=$(date +%s)
 
-if [ ! -d $LOGS_DIR ]; then
+if [ ! -d "$LOGS_DIR" ]; then
    echo -e "$LOGS_DIR $R Directory Not Exist $N" 
    exit 1
 fi
@@ -17,10 +17,15 @@ fi
 FILES_TO_DELETE=$(find $LOGS_DIR -name "*.log" -mtime +14)
 # echo "$FILES_TO_DELETE"
 
+if [ -z "$FILES_TO_DELETE" ]; then
+   echo "No old logs to delete"
+   exit 0
+fi
+
 while IFS= read -r filepath; 
 do
    echo "Deleting File: $filepath" | tee -a $LOGS_FILE
-   rm -f $filepath
+   rm -f "$filepath"
    echo "Deleted Files: $filepath" | tee -a $LOGS_FILE
 done <<< $FILES_TO_DELETE
 
