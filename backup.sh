a4b2ae7b18d66fe938836b6878d3d28d855874d6
pavankumar0816 <manikantapavankumarpuppala@gmail.com>
2026-06-26 21:45:10 +0530

    updated

--- backup.sh
+++ backup.sh
@@ -2,34 +2,44 @@
 
 userid=$(id -u)
 
+# Log Details
 LOGS_FOLDER="/var/log/shellscript"
 SCRIPT_NAME=$(basename "$0" .sh)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+# Color codes
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+# Arguments passed while executing the script
 SOURCE_DIR=$1
 DEST_DIR=$2
-DAYS=${3:-14}
+DAYS=${3:-14} # (optional) Default days is 14 days if not provided
 
+
+# Validation function
 log(){
     echo -e "$(date "+%Y-%m-%d ::: %H:%M:%S") |  $1" | tee -a $LOG_FILE
 }
 
+# Root user validation
 if [ $userid -ne 0 ]; then
     log "$R Please run this script with root user access $N" 
     exit 1
 fi
 
+# Create logs directory if it doesn't exist
 mkdir -p $LOGS_FOLDER
 
+# Usage information
 USAGE(){
     log "$R USAGE: sudo backup <source-dir> <dest-dir> <days>[default 14 days] $N"
     exit 1
 }
 
+# Validate input arguments
 if [ $# -lt 2 ]; then
     USAGE
 fi
@@ -44,26 +54,28 @@ if [ ! -d "$DEST_DIR" ]; then
     exit 1
 fi
 
-log "Backup Started"
-log "Source Directory: $SOURCE_DIR"
-log "Destination Directory: $DEST_DIR"
-log "Days: $DAYS"
 
-## Find the files
+## Find old log files
 FILES=$(find $SOURCE_DIR -name "*.log" -type f -mtime +$DAYS)
 
 if [ -z "$FILES" ]; then
     log "No files to Archive, $Y Skipping $N" 
 else
     log "Files found to Archive: $FILES" 
+
     TIMESTAMP=$(date +%F-%H-%M-%S)
     ZIP_FILE=$DEST_DIR/app-logs-"$TIMESTAMP".tar.gz
+
     log "Archive name: $ZIP_FILE"
+
+    # Create the archive
     tar -zcvf "$ZIP_FILE" $FILES
 
     if [ -f "$ZIP_FILE" ]; then
-        log "Archieval is $G ... Success ...$N"
+       log "Archieval is $G ... Success ...$N"
 
+       
+        # Delete archived log files
         while IFS= read -r filepath; do
         log "Deleting File: $filepath"
         rm -f $filepath
