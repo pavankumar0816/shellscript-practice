4e129bd86de1db083fac87dfff504847d57fd647
pavankumar0816 <manikantapavankumarpuppala@gmail.com>
2026-06-26 12:38:49 +0530

    backup

--- backup.sh
+++ backup.sh
@@ -56,11 +56,10 @@ if [ -z "$FILES" ]; then
     log "No files to Archive, $Y Skipping $N" 
 else
     log "Files found to Archive: $FILES" 
-    TIMESTAMP=$(date)
-    echo "Time:$TIMESTAMP"
+    TIMESTAMP=$(date +%F::%H-%M-%S)
     ZIP_FILE=$DEST_DIR/app-logs-"$TIMESTAMP".tar.gz
     log "Archive name: $ZIP_FILE"
-    # tar -zcvf "$ZIP_FILE" $FILES
+    tar -zcvf "$ZIP_FILE" $FILES
 
 fi
 
