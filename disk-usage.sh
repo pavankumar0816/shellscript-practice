9656d219616bd8d65e79e20ee8179eeb001df4d1
pavankumar0816 <manikantapavankumarpuppala@gmail.com>
2026-06-27 15:10:14 +0530

    updated

--- disk-usage.sh
+++ disk-usage.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
+#Colors
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+# Getting disk usage, excluding header
 DISK_USAGE=$(df -hT | grep -v Filesystem)
-USAGE_THRESHOLD=3
- 
-# echo "$DISK_USAGE"
 
+#For example i have taken 3
+USAGE_THRESHOLD=3
+
+# Loop through each disk entry 
 while IFS= read -r line;
 do
-#    USAGE=$(echo "$line" | awk '{print $6}' | cut -d "%" -f1)
-#    PARTITION=$(echo "$line" | awk '{print $7}')
-
-    read -r USAGE PARTITION <<< "$(echo "$line" | awk '{print $6, $7}')"
-    USAGE=${USAGE%\%}
+    USAGE=$(echo "$line" | awk '{print $6}' | cut -d "%" -f1)
+    PARTITION=$(echo "$line" | awk '{print $7}')
 
     if [ "$USAGE" -gt "$USAGE_THRESHOLD" ]; then
         MESSAGE+="$R High disk usage on   $N"$PARTITION": $R "$USAGE"% $N  \n"
