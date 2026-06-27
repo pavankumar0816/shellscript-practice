11ab9eb8745fd6a28119d6695e4db41862cb93c9
pavankumar0816 <manikantapavankumarpuppala@gmail.com>
2026-06-27 14:40:16 +0530

    disk-usage

--- disk-usage.sh
+++ disk-usage.sh
@@ -8,4 +8,11 @@ N="\e[0m"
 DISK_USAGE=$(df -hT | grep -v Filesystem)
 USAGE_THRESHOLD=3
 
- echo "$DISK_USAGE"
+echo "$DISK_USAGE"
+
+while IFS= read -r line;
+do
+   USAGE=$($DISK_USAGE | awk '{print $6}')
+   echo "$USAGE"  
+
+done <<< $DISK_USAGE
