9d9e9c969ecc1669deb14f7ca14bb1432d297a89
pavankumar0816 <manikantapavankumarpuppala@gmail.com>
2026-06-27 14:45:24 +0530

    disk-usage

--- disk-usage.sh
+++ disk-usage.sh
@@ -7,12 +7,20 @@ N="\e[0m"
 
 DISK_USAGE=$(df -hT | grep -v Filesystem)
 USAGE_THRESHOLD=3
+# MESSAGE=""
 
 echo "$DISK_USAGE"
 
 while IFS= read -r line;
 do
-   USAGE=$(echo $line | awk '{print $6}')
-   echo "$USAGE"  
+   USAGE=$(echo $line | awk '{print $6}' | cut -d "%" -f1)
+   echo "$USAGE" 
+   PARTITION=$(echo $line | awk '{print $7}')
+
+    if [ $USAGE -gt $USAGE_THRESHOLD ]; then
+        MESSAGE="High disk usage on $PARTITION:$USAGE% <br>"
+        echo $MESSAGE
+    fi
+    
 
 done <<< $DISK_USAGE
